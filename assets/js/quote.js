bda3309dda6717d342608bf265f34f03679c8f4b
Denis Lafontant <icebox827@gmail.com>
2021-10-08 21:23:32 -0400

    create loadpage function

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,12 +1,37 @@
 import api from './api';
 
-const quoteGenrator = () => {
+const quoteGenerator = () => {
   const modalBox = document.createElement('card');
+  const text = document.createElement('h1');
+  const author = document.createElement('h2');
+
   modalBox.classList.add('modal-card');
+  text.classList.add('text');
+  author.classList.add('author');
 
+  text.setAttribute('id', 'text');
+  author.setAttribute('id', 'author');
 
+  modalBox.appendChild(text);
+  modalBox.appendChild(author);
+
+  return quoteGenerator;
 };
 
-async function loadpage() {
-    
+const displayQuote = (data) => {
+  const getText = document.getElementById('text');
+  const getAuthor = document.getElementById('author');
+
+  getText.innerHTML = data.text;
+  getAuthor.innerHTML = data.author;
 }
+
+async function loadpage() {
+  const popup = document.getElementById('popup');
+
+  const fetchdata = await api();
+  (displayQuote(fetchdata));
+  popup.appendChild(quoteGenerator());
+};
+
+export default loadpage;
