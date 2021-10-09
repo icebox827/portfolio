f6d767cc3b268119b60bed27d75c636906b00c45
Denis Lafontant <icebox827@gmail.com>
2021-10-08 20:24:31 -0400

    Create async function to consume API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,17 +1,9 @@
-var axios = require("axios").default;
+const api = async () => {
+  const basedUrl = 'https://type.fit/api/quotes'
 
-var options = {
-  method: 'GET',
-  url: 'https://healthruwords.p.rapidapi.com/v1/quotes/',
-  // params: {t: 'Wisdom', maxR: '1', size: 'medium', id: '731'},
-  headers: {
-    'x-rapidapi-host': 'healthruwords.p.rapidapi.com',
-    'x-rapidapi-key': '413dcda1b3msh390a06fcb8b7519p1aa241jsn28de708ce169'
-  }
+  const response = await fetch(basedUrl, { mode: 'cors' }).then((e) => e.json());
+
+  return response;
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+export default api;
---
+++ assets/js/quote.js
@@ -0,0 +1,12 @@
+import api from './api';
+
+const quoteGenrator = () => {
+  const modalBox = document.createElement('card');
+  modalBox.classList.add('modal-card');
+
+
+};
+
+async function loadpage() {
+    
+}
