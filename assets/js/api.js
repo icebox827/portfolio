14b318e1553bdbf879526fa1bf303afc697bb654
Denis <icebox827@gmail.com>
2022-01-23 18:11:41 -0400

    Update app

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,9 @@
 const api = async () => {
   const basedUrl = 'https://type.fit/api/quotes'
 
-  const response = await fetch(basedUrl, { mode: 'cors' }).then((e) => e.json()
-  .catch((e) => console.log(error)));
+  const response = await fetch(basedUrl, { mode: 'cors' })
+    .then((e) => e.json()
+    .catch((e)));
 
   console.log(response)
   return response;
